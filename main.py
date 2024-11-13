ca115abe3ef9bb9a84b896b47bbecebab6453f01
Yomnaa Muhammedd <yomnaamuhammedd@Yomnaas-MacBook-Air.local>
2024-11-14 00:17:24 +0200

    Modified main file to include base route

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from src.Routes import base ,upload_document_api
+
+load_dotenv()
 
 app = FastAPI()
+
+app.include_router(base.base_router)
+app.include_router(upload_document_api.document_upload_router)
+
