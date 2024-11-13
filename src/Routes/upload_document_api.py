3654a4be140c9953a6c9d726347e8493b1f90758
Yomnaa Muhammedd <yomnaamuhammedd@Yomnaas-MacBook-Air.local>
2024-11-14 00:20:53 +0200

    updated upload_document api

--- src/Routes/upload_document_api.py
+++ src/Routes/upload_document_api.py
@@ -1,54 +1,28 @@
 import os
-from fastapi import APIRouter,UploadFile,File,HTTPException
-from Services.document_service import DocumentService
-from Helpers.Enums.Response import ResponseStatus, ResponseMessage,ErrorType
+from fastapi import APIRouter,UploadFile,File,HTTPException,status
+from fastapi.responses import JSONResponse
+from src.Services.document_service import DocumentService
+from src.Helpers.Enums.ResponseEnum import ResponseStatus, ResponseMessage,ErrorType
 from pydantic import BaseModel
 
 document_upload_router = APIRouter()
 
-class DocumentUploadResponse(BaseModel):
-    status: ResponseStatus
-    message: ResponseMessage
-
-class ErrorResponse(BaseModel):
-    error: ErrorType
-    detail: str
-
-UPLOAD_DIR = "./uploaded_files"
-if not os.path.exists(UPLOAD_DIR):
-    os.makedirs(UPLOAD_DIR)
-
 @document_upload_router.post("/upload")
 async def upload_document(file:UploadFile = File(...)):
+
     document_service = await DocumentService()
+    valid,validation_signal = await document_service.validate_uploaded_file(file=file)
 
-    if file.content_type not in ["application/pdf","text/plain"]:
-        raise HTTPException(status_code=400,detail="Only Text inputs and PDFs are allowed") 
-    
-    try:
-        # By encoding content
-        # file_content = await file.read()  # Read file content
-        # response = await document_service.process_document(file.filename, file_content)
-
-        # By reading file path
-        file_path = os.path.join(UPLOAD_DIR, file.filename)
-        with open(file_path, "wb") as f:
-            content = await file.read()
-            f.write(content)
+    if valid:
+       response,num_chunks = await document_service.run(file=file)
+       return JSONResponse(status_code=status.HTTP_200_OK,
+                            content={"response":response,
+                                     "number of chunks inserted": num_chunks} )
+    else:
+     return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
+                            content={"signal":validation_signal} )
         
-        # Process the file using DocumentService
-        result = await document_service.run(file_path)
-
-        # After processing, delete the file
-        os.remove(file_path)
-
-        return DocumentUploadResponse(
-            status=ResponseStatus.SUCCESS, 
-            message=ResponseMessage.FILE_UPLOADED
-        )
-    
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=ErrorType.INTERNAL_ERROR)
+  
     
 
       
