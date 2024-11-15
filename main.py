0b297099d2579a0e11ca02b770d3296d85ba8584
Yomnaa Muhammedd <yomnaamuhammedd@Yomnaas-MacBook-Air.local>
2024-11-15 02:03:49 +0200

    updated main to incliude chatbot api

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
-from src.Routes import base ,upload_document_api
+from src.Routes import base ,upload_document_api,chat_api
 
 load_dotenv()
 
@@ -8,4 +8,8 @@ app = FastAPI()
 
 app.include_router(base.base_router)
 app.include_router(upload_document_api.document_upload_router)
+app.include_router(chat_api.chat_api_router)
 
+@app.get("/")
+async def root():
+    return {"message": "Welcome Slash Chatbot"}
